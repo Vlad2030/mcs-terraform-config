a73545db9fc74f3c6c7b944de4d3d5c7434065b8
vladdd0 <vlad123123v@gmail.com>
2023-04-15 18:30:13 +0300

    arch

--- terraform-install.sh
+++ terraform-install.sh
@@ -1,4 +1,5 @@
 wget http://hub.mcs.mail.ru/repository/terraform-binary/mirror/latest/terraform_1.1.9_linux_amd64.zip
 unzip terraform_1.1.9_linux_amd64.zip
 rm -f terraform_1.1.9_linux_amd64.zip
+echo ""
 echo "Terraform installed! Write ./terraform to use."
