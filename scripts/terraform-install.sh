b281206c8a717402f0fab8bf020fc8f12ecb00b4
vladdd0 <vlad123123v@gmail.com>
2023-04-20 05:16:09 +0300

    Update terraform-install.sh

--- scripts/terraform-install.sh
+++ scripts/terraform-install.sh
@@ -2,6 +2,6 @@ wget http://hub.mcs.mail.ru/repository/terraform-binary/mirror/latest/terraform_
 unzip terraform_1.1.9_linux_amd64.zip
 rm -f terraform_1.1.9_linux_amd64.zip
 chmod +x terraform
-cp terraform /usr/terraform
+cp terraform /usr/bin/
 echo && echo
 echo "Terraform installed! Write ./terraform to use."
